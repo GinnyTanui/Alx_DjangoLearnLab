dfdf711dddb5c913619efb8830febc4d9692dd8d
GinnyTanui <tanuiginny@gmail.com>
2025-07-20 10:56:29 +0300

    M

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -20,7 +20,7 @@ except Author.DoesNotExist:
 #query 2
 library_name = "Hometown Library"
 try:
-     library = Library.objects.get(library_name)
+     library = Library.objects.get(name=library_name)  #
      books_in_library = library.books.all()
      print(f"\nBooks in {library_name}:")
      for book in books_in_library:
