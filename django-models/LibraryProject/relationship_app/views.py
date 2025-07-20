05dc0f1fdf3f52a2054ad3e83fe401bc83415c87
GinnyTanui <tanuiginny@gmail.com>
2025-07-20 17:10:42 +0300

    thir

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,7 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout 
 from django.contrib.auth.decorators import login_required
+from django.views import View
 # Create your views here.
 def list_books(request):
     books = Book.objects.all()
@@ -16,29 +17,41 @@ class LibraryDetailView(DetailView):
     context_object_name = "library" 
 
 #Registering 
-def register_view(request):
-    if request.method == 'POST':
+class RegisterView(View):
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, 'relationship_app/register.html', {'form': form})
+
+    def post(self, request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('login')
-    else:
-        form = UserCreationForm()
-    return render(request, 'relationship_app/register.html', {'form': form})
+        return render(request, 'relationship_app/register.html', {'form': form})
+##def register_view(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             return redirect('login')
+#     else:
+#         form = UserCreationForm()
+#     return render(request, 'relationship_app/register.html', {'form': form})
 
-def login_view(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('logout') #uses authentication form to check user and password, if its correct, gets the user and logs them in automatically
-    else:
-        form = AuthenticationForm()
+# def login_view(request):
+#     if request.method == 'POST':
+#         form = AuthenticationForm(request, data=request.POST)
+#         if form.is_valid():
+#             user = form.get_user()
+#             login(request, user)
+#             return redirect('logout') #uses authentication form to check user and password, if its correct, gets the user and logs them in automatically
+#     else:
+#         form = AuthenticationForm()
 
-    return render(request, 'relationship_app/login.html', {'form': form}) 
+#     return render(request, 'relationship_app/login.html', {'form': form}) 
 
-def logout_view(request):
-    logout(request) 
-    return render(request, 'relationship_app/logout.html')
+# def logout_view(request):
+#     logout(request) 
+#     return render(request, 'relationship_app/logout.html')
