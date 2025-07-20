7e87e58fa90e1ebf8556db1d5c43ee1c1ad35c26
GinnyTanui <tanuiginny@gmail.com>
2025-07-20 18:34:16 +0300

    fourth

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here. 
 from django.db import models
-
+from django.contrib.auth.models import User
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
@@ -34,3 +34,14 @@ class Librarian(models.Model):
         return self.title  # ✅ Only return title
 
 
+class UserProfile(models.Model):
+    ROLE_CHOICES = (
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, logout 
 from django.contrib.auth.decorators import login_required
 from django.views import View
+from django.contrib.auth.decorators import user_passes_test 
+from .models import UserProfile
 # Create your views here.
 def list_books(request):
     books = Book.objects.all()
@@ -67,3 +69,25 @@ class register(View):  # 👈 Name it lowercase 'register' so checker sees views
 # def logout_view(request):
 #     logout(request) 
 #     return render(request, 'relationship_app/logout.html')
+# relationship_app/views.p
+
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html')
